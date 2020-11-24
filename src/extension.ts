fcac4afa12d538fd0be1bff1f686aa8dcdff9b98
Kyle Nanakdewa <KyleN@live.ca>
2020-11-23 23:00:42 -0500

    hashes are now correctly unsigned

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,13 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('sporehash.copySelectionHash', () => {
 		let hashString = copySelectionHashString();
 		vscode.window.showInformationMessage(`Copied Spore Hash ${hashString} to clipboard.`);
+
+		/*//TESTING
+		let selection = getSelection();
+		if(selection!==undefined && selection.length===8){
+			let hash = parseInt(selection, 16);
+			let result = bruteForceHashDecode(hash);
+		}*/
 	}));
 
 	// Create status bar item
@@ -34,19 +41,26 @@ function calculateHash(text: string): number {
 	return hash;
 }
 
-/**
- * Calculates the Spore FNV hash for the current selected text in the editor.
- * @returns the hash, or undefined if no editor is active
- */
-function calculateSelectionHash(): number | undefined {
+function getSelection(): string | undefined {
 	let editor = vscode.window.activeTextEditor;
 	if (editor !== undefined) {
 		let selection = editor?.selection;
 		if (!selection.isEmpty) {
 			let text = editor?.document.getText(selection);
-			let hash = calculateHash(text);
-			return hash;
+			return text;
 		}
+	}
+}
+
+/**
+ * Calculates the Spore FNV hash for the current selected text in the editor.
+ * @returns the hash, or undefined if no editor is active
+ */
+function calculateSelectionHash(): number | undefined {
+	let selection = getSelection();
+	if (selection !== undefined) {
+		let hash = calculateHash(selection);
+		return hash;
 	}
 }
 
@@ -56,8 +70,10 @@ function calculateSelectionHash(): number | undefined {
  */
 function calculateSelectionHashString(): string | undefined {
 	let hash = calculateSelectionHash();
-	let hexString = hash?.toString(16).toUpperCase();
-	return hexString;
+	if (hash !== undefined) {
+		let hexString = (hash >>> 0).toString(16).toUpperCase();
+		return hexString;
+	}
 }
 
 /**
@@ -79,11 +95,35 @@ function updateStatusBarHash(): void {
 	let hashString = calculateSelectionHashString();
 	if (hashString !== undefined) {
 		// Add space in middle for readability
-		let word1 = hashString.substr(0,4);
-		let word2 = hashString.substr(4,4);
+		let word1 = hashString.substr(0, 4);
+		let word2 = hashString.substr(4, 4);
 		statusBarHash.text = `Spore Hash: ${word1} ${word2}`;
 		statusBarHash.show();
 	} else {
 		statusBarHash.hide();
 	}
+}
+
+
+// TESTING STUFF
+function bruteForceHashDecode(hash: number): string | undefined {
+	let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+	alphabet.forEach(letter1 => {
+		alphabet.forEach(letter2 => {
+			alphabet.forEach(letter3 => {
+				let testString = `${letter1}${letter2}${letter3}`;
+
+				let testHash = calculateHash(testString);
+				if (testHash === hash) {
+					vscode.window.showInformationMessage(`Decoded ${testHash.toString(16)} to "${testString}".`);
+					return testString;
+				} else {
+					//vscode.window.showInformationMessage(`Decoding, now testing "${testString}" (${testHash.toString(16)}).`);
+				}
+			});
+		});
+	});
+
+	return undefined;
 }
